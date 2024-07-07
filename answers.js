acc4560ac4fbfa179f1fd8d852f7cffb6c738e18
operan38 <gorbynov2000@yandex.ru>
2024-07-07 17:23:53 +0500

    add blur, add questNumber

--- answers.js
+++ answers.js
@@ -1,8 +1,13 @@
 const QUEST_KEY = 'q';
 
+const url = new URL(window.location.href);
+const urlParams = new URLSearchParams(url.search);
 const $answer = document.getElementById("answer");
+const $questNumber = document.getElementById("quest-number");
 const $next = document.getElementById("next");
 const $prev = document.getElementById("prev");
+
+let isBlur = false;
 
 window.history.pushState = new Proxy(window.history.pushState, {
   apply: (target, thisArg, argArray) => {
@@ -13,47 +18,80 @@ window.history.pushState = new Proxy(window.history.pushState, {
 });
 
 function setQuest(num) {
-  const url = new URL(window.location.href);
-  const urlParams = new URLSearchParams(url.search);
   urlParams.set(QUEST_KEY, num);
   url.search = urlParams.toString();
   history.pushState({}, "", url);
 };
 
+function getQuestNumber() {
+  return urlParams.get(QUEST_KEY);
+}
+
+function isNumberUncorrect(num) {
+  return num === "" || num === null || num === undefined ? true : false;
+}
+
 function next() {
   console.log('next');
-  const url = new URL(window.location.href);
-  const questNumber = parseInt(url.searchParams.get(QUEST_KEY));
+  $questNumber.classList.remove("fadeIn");
+  const questNumber = getQuestNumber();
+  if (isNumberUncorrect(questNumber)) return;
 
-  setQuest(questNumber + 1);
+  setQuest(parseInt(questNumber) + 1);
 }
 
 function prev() {
   console.log('prev');
-  const url = new URL(window.location.href);
-  const questNumber = parseInt(url.searchParams.get(QUEST_KEY));
+  $questNumber.classList.remove("fadeIn");
+  const questNumber = getQuestNumber();
+  if (isNumberUncorrect(questNumber)) return;
 
-  setQuest(questNumber - 1);
+  setQuest(parseInt(questNumber) - 1);
+}
+
+function addBlur() {
+  isBlur = true;
+  $answer.classList.add("blur");
+  $answer.classList.remove("unblur");
+}
+
+function removeBlur() {
+  isBlur = false;
+  $answer.classList.remove("blur");
+  $answer.classList.add("unblur");
 }
 
 function update() {
-  const url = new URL(window.location.href);
-  const questNumber = url.searchParams.get(QUEST_KEY);
+  addBlur();
+  const questNumber = getQuestNumber();
 
-  if (questNumber === "" || questNumber === null || questNumber === undefined) {
+  if (isNumberUncorrect(questNumber)) {
     const txt = "Ответа на этот вопрос не существует :(";
     document.title = txt;
     $answer.innerHTML = txt;
+    removeBlur();
     return;
   }
+  else if (questNumber <= 1) {
+    $prev.style.display = 'none';
+  }
+  else {
+    $prev.style.display = 'block';
+  }
 
-  document.title = `Ответ на ${questNumber} вопрос`;
+  setTimeout(() => {
+    $questNumber.classList.add("fadeIn");
+  }, 1);
+
+  $questNumber.innerHTML = `Вопрос № ${questNumber}`;
   $answer.innerHTML = `Ответ на ${questNumber} вопрос`;
 }
 
 window.onload = function() {
+  $questNumber.classList.add("fadeIn");
   update();
 
   $next.addEventListener("click", next);
   $prev.addEventListener("click", prev);
+  $answer.addEventListener("click", removeBlur);
 }
